2ffeeef096d5201149dd0dff72697a9d34b96199
Web-Entwickler <pixpox21@bk.ru>
2023-03-30 13:28:08 +0200

    feat: add orders for users

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,6 +1,8 @@
 package user;
 
+import cart.ProductCart;
 import exceptions.NewIllegalException;
+import product.Product;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -8,6 +10,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.KeySpec;
+import java.util.List;
 
 public class User {
     private String name;
@@ -15,7 +18,10 @@ public class User {
 
     private String password;
 
+    private List<ProductCart> orders;
+
     private boolean admin = false;
+
 
     public User(String name, String email, String password) {
         if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
@@ -58,11 +64,20 @@ public class User {
         return password;
     }
 
+    public List<ProductCart> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<ProductCart> orders) {
+        this.orders = orders;
+    }
+
     @Override
     public String toString() {
         return "User {" +
                 "name='" + name + '\'' +
                 ", email='" + email + '\'' +
+                ", orders='" + orders.size() + '\'' +
                 ", admin=" + admin +
                 '}';
     }
